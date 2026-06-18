abe91851cc60c460f5fa65faa881f903ae311f02
halili <lxpwcd@mail.ustc.edu.cn>
2026-06-18 15:56:53 +0800

    Support phased CPU distributed hvec

--- example/test_virtual_distnetwork.jl
+++ example/test_virtual_distnetwork.jl
@@ -5,6 +5,7 @@ _ratio = length(ARGS) >= 4 ? parse(Float64, ARGS[4]) : 1.0
 _vk    = length(ARGS) >= 5 ? parse(Int, ARGS[5]) : 3
 _seed  = length(ARGS) >= 6 ? parse(Int, ARGS[6]) : 1234
 _ntry  = length(ARGS) >= 7 ? parse(Int, ARGS[7]) : 64
+_phases = length(ARGS) >= 8 ? parse(Int, ARGS[8]) : 1
 
 ENV["OMP_NUM_THREADS"] = _nts
 delete!(ENV, "OMP_PROC_BIND")
@@ -26,11 +27,11 @@ if abspath(PROGRAM_FILE) == @__FILE__
     build(mole)
 
     ham = JW_hamiltonian(mole)
-    funcs, basis = DistributedFunctions(mole, ham, comm; virtual_k=_vk, virtual_seed=_seed, virtual_ntry=_ntry)
+    funcs, basis = DistributedFunctions(mole, ham, comm; virtual_k=_vk, virtual_seed=_seed, virtual_ntry=_ntry, num_phases=_phases)
 
     my_rank == 0 && println("="^60)
     my_rank == 0 && println("=== Virtual-symmetry DistributedFunctions Test ===")
-    my_rank == 0 && @printf("Molecule: %s / %s  virtual_k: %d  ntry: %d  dim: %d\n", _name, _basis, _vk, _ntry, basis.dim)
+    my_rank == 0 && @printf("Molecule: %s / %s  virtual_k: %d  ntry: %d  phases: %d  dim: %d\n", _name, _basis, _vk, _ntry, _phases, basis.dim)
     my_rank == 0 && println("="^60)
 
     v = funcs.get_hf(mole.nelec)
