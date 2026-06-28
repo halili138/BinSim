ce74de99cb345f4ccf3c8ad68f944b63d60e49b8
halili <lxpwcd@mail.ustc.edu.cn>
2026-06-28 11:21:08 +0800

    Avoid unused original_idx loads in hvec

--- src/include/cuda_interface.cuh
+++ src/include/cuda_interface.cuh
@@ -213,8 +213,13 @@ static inline void launch_cuda_single_group_rank(
         return;
     }
 
-    dim3 grid(basis_slice.num_blocks, max_tasks);
-    cuda_single_group_sharedtile_kernel<Rank, TypeCode, Ti, Tv, Op><<<grid, block_size>>>(basis_slice, groups, pos, op);
+    constexpr int MAX_GRID_Y = 65535;
+    for (int task_offset = 0; task_offset < max_tasks; task_offset += MAX_GRID_Y)
+    {
+        const int tasks_this_launch = std::min(MAX_GRID_Y, max_tasks - task_offset);
+        dim3 grid(basis_slice.num_blocks, tasks_this_launch);
+        cuda_single_group_sharedtile_kernel<Rank, TypeCode, Ti, Tv, Op><<<grid, block_size>>>(basis_slice, groups, pos, op, task_offset);
+    }
 }
 
 template <int TypeCode, typename Ti, typename Tv, typename Op>
@@ -534,6 +539,7 @@ struct CudaHVecMultiGroupOp
     static constexpr bool SkipRealDiagonal = false;
     static constexpr bool SkipLowerBlocks = false;
     static constexpr bool SkipSameBlockReverse = false;
+    static constexpr bool UsesOriginalIdx = false;
 
     const Tv *src_vec;
     Tv *dst_vec;
@@ -588,6 +594,7 @@ struct CudaBatchGradMultiGroupOp
     static constexpr bool SkipRealDiagonal = true;
     static constexpr bool SkipLowerBlocks = true;
     static constexpr bool SkipSameBlockReverse = true;
+    static constexpr bool UsesOriginalIdx = true;
 
     const double *thetas;
     const Tv *lp;
